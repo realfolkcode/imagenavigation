25401e01bf24641c1bf0dac5dc00bafb24408e31
realfolkcode <wtfomgrofl@mail.ru>
2022-10-18 14:12:02 +0300

    Remove passing device

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torch
 
-def process_image(feature_extractor, model, image, device):
+def process_image(feature_extractor, model, image):
+    device = model.device
     inputs = feature_extractor(image, return_tensors="pt").to(device)
 
     with torch.no_grad():
